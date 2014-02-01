6e45158179bf04c01b6cc17664004050f6bf5079
Gavin McDonald <gavinmcdoh@gmail.com>
2014-02-01 10:39:33 -0500

    smarter setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-ln -s ./DotFiles/.gitconfig .
-ln -s ./DotFiles/.gitignore .
-ln -s ./DotFiles/.vimrc .
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+ln -s $SCRIPTPATH/.gitconfig ~
+ln -s $SCRIPTPATH/.gitignore ~
+ln -s $SCRIPTPATH/.vimrc ~
+
+mkdir -p ~/.vim/backups
